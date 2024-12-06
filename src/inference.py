045de6a0f0c916e197c566d9f49c0c8a350cfe48
ds-wook <leewook94@gmail.com>
2024-12-06 14:02:55 +0900

    feat: add evaluate code

---
+++ src/inference.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import hydra
+import lightgbm as lgb
+import numpy as np
+import pandas as pd
+from catboost import CatBoostRanker
+from omegaconf import DictConfig
+from prettytable import PrettyTable
+
+from data import load_dataset, load_test_dataset
+from model import TreeModel
+
+
+def candidate_generation(
+    user_id: int, candidate_pool: list[str], user_to_anime_map: dict[int, list[str]], N: int
+) -> tuple[list[str], list[str]]:
+    already_interacted = user_to_anime_map[user_id]
+    candidates = list(set(candidate_pool) - set(already_interacted))
+    return already_interacted, np.random.choice(candidates, size=N)
+
+
+def generate_predictions(
+    cfg: DictConfig,
+    user_id: int,
+    user_2_anime_map: dict[int, list[str]],
+    candidate_pool: list[str],
+    feature_columns: list[str],
+    anime_id_2_name_map: dict[int, list[str]],
+    ranker: TreeModel,
+    N: int = 100,
+) -> pd.DataFrame:
+    anime_info_df_final, _, user_info = load_dataset(cfg)
+    already_liked, candidates = candidate_generation(user_id, candidate_pool, user_2_anime_map, N=10000)
+    candidates_df = pd.DataFrame(data=pd.Series(candidates, name="anime_id"))
+    features = anime_info_df_final.merge(candidates_df)
+    features["user_id"] = user_id
+    features = features.merge(user_info)
+
+    already_liked = list(already_liked)
+    if len(already_liked) < len(candidates):
+        append_list = np.full(fill_value=-1, shape=(len(candidates) - len(already_liked)))
+        already_liked.extend(list(append_list))
+
+    predictions = pd.DataFrame(index=candidates)
+    predictions["name"] = np.array([anime_id_2_name_map.get(id_) for id_ in candidates])
+    predictions["score"] = ranker.predict(features[feature_columns])
+    predictions = predictions.sort_values(by="score", ascending=False).head(N)
+
+    predictions[f"already_liked - sample[{N}]"] = [
+        anime_id_2_name_map.get(_id) for _id in already_liked[0 : len(predictions)]
+    ]
+    return predictions
+
+
+@hydra.main(config_path="../config/", config_name="inference", version_base="1.3.1")
+def _main(cfg: DictConfig):
+    user_2_anime_map, candidate_pool, anime_id_2_name_map = load_test_dataset(cfg)
+
+    ranker = (
+        lgb.Booster(model_file=Path(cfg.models.model_path) / f"{cfg.models.results}.model")
+        if cfg.models.name == "lightgbm"
+        else CatBoostRanker().load_model(Path(cfg.models.model_path) / f"{cfg.models.results}.model")
+    )
+
+    predictions = generate_predictions(
+        cfg=cfg,
+        user_id=123,
+        user_2_anime_map=user_2_anime_map,
+        candidate_pool=candidate_pool,
+        feature_columns=cfg.data.features,
+        anime_id_2_name_map=anime_id_2_name_map,
+        ranker=ranker,
+        N=cfg.N,
+    )
+
+    table = PrettyTable()
+    table.field_names = ["Anime Name", "Already Liked", "Predicted Score"]
+
+    for _, row in predictions.iterrows():
+        table.add_row([row["name"], row[f"already_liked - sample[{cfg.N}]"], f"{row['score']:.3f}"])
+
+    print(table)
+
+
+if __name__ == "__main__":
+    _main()
---
+++ src/train.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import warnings
+from pathlib import Path
+
+import hydra
+from omegaconf import DictConfig
+
+from data import load_train_dataset
+from model import bulid_model
+
+
+@hydra.main(config_path="../config/", config_name="train", version_base="1.2.0")
+def _main(cfg: DictConfig):
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=UserWarning)
+
+        # load dataset
+        X_train, X_test, y_train, y_test = load_train_dataset(cfg)
+
+        # choose trainer
+        trainer = bulid_model(cfg)
+
+        # train model
+        ranker = trainer.fit(X_train, y_train, X_test, y_test)
+
+        # save model
+        ranker.save_model(Path(cfg.models.model_path) / f"{cfg.models.results}.model")
+
+
+if __name__ == "__main__":
+    _main()
